cd0f66ca67a8a058c7cd515203a5ff2f1df96f20
yzzzzun <yzzzzun@gmail.com>
2023-01-18 20:47:42 +0900

    test: bookService test

--- src/main/java/com/group/libraryapp/domain/user/loanhistory/UserLoanHistory.java
+++ src/main/java/com/group/libraryapp/domain/user/loanhistory/UserLoanHistory.java
@@ -1,44 +1,51 @@
 package com.group.libraryapp.domain.user.loanhistory;
 
-import com.group.libraryapp.domain.user.User;
+import static javax.persistence.GenerationType.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
-import static javax.persistence.GenerationType.IDENTITY;
+import com.group.libraryapp.domain.user.User;
 
 @Entity
 public class UserLoanHistory {
 
-  @Id
-  @GeneratedValue(strategy = IDENTITY)
-  private Long id;
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
+	private Long id;
 
-  @ManyToOne
-  private User user;
+	@ManyToOne
+	private User user;
 
-  private String bookName;
+	private String bookName;
 
-  private boolean isReturn;
+	private boolean isReturn;
 
-  public UserLoanHistory() {
+	public UserLoanHistory() {
 
-  }
+	}
 
-  public UserLoanHistory(User user, String bookName, boolean isReturn) {
-    this.user = user;
-    this.bookName = bookName;
-    this.isReturn = isReturn;
-  }
+	public UserLoanHistory(User user, String bookName, boolean isReturn) {
+		this.user = user;
+		this.bookName = bookName;
+		this.isReturn = isReturn;
+	}
 
-  public String getBookName() {
-    return this.bookName;
-  }
+	public String getBookName() {
+		return this.bookName;
+	}
 
-  public void doReturn() {
-    this.isReturn = true;
-  }
+	public void doReturn() {
+		this.isReturn = true;
+	}
 
+	public User getUser() {
+		return user;
+	}
+
+	public boolean isReturn() {
+		return isReturn;
+	}
 }
--- src/main/java/com/group/libraryapp/dto/book/request/BookLoanRequest.java
+++ src/main/java/com/group/libraryapp/dto/book/request/BookLoanRequest.java
@@ -2,15 +2,20 @@ package com.group.libraryapp.dto.book.request;
 
 public class BookLoanRequest {
 
-  private String userName;
-  private String bookName;
+	private String userName;
+	private String bookName;
 
-  public String getUserName() {
-    return userName;
-  }
+	public BookLoanRequest(String userName, String bookName) {
+		this.userName = userName;
+		this.bookName = bookName;
+	}
 
-  public String getBookName() {
-    return bookName;
-  }
+	public String getUserName() {
+		return userName;
+	}
+
+	public String getBookName() {
+		return bookName;
+	}
 
 }
