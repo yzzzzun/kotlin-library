93a332ca6dab4dee370bcf742bdfce0e74ac7816
yzzzzun <yzzzzun@gmail.com>
2023-01-25 20:21:02 +0900

    refactor: user service 리팩토링

--- src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -1,40 +1,47 @@
 package com.group.libraryapp.controller.user;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
 import com.group.libraryapp.service.user.UserService;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 public class UserController {
 
-  private final UserService userService;
+	private final UserService userService;
 
-  public UserController(UserService userService) {
-    this.userService = userService;
-  }
+	public UserController(UserService userService) {
+		this.userService = userService;
+	}
 
-  @PostMapping("/user")
-  public void saveUser(@RequestBody UserCreateRequest request) {
-    userService.saveUser(request);
-  }
+	@PostMapping("/user")
+	public void saveUser(@RequestBody UserCreateRequest request) {
+		userService.saveUser(request);
+	}
 
-  @GetMapping("/user")
-  public List<UserResponse> getUsers() {
-    return userService.getUsers();
-  }
+	@GetMapping("/user")
+	public List<UserResponse> getUsers() {
+		return userService.getUsers();
+	}
 
-  @PutMapping("/user")
-  public void updateUserName(@RequestBody UserUpdateRequest request) {
-    userService.updateUserName(request);
-  }
+	@PutMapping("/user")
+	public void updateUserName(@RequestBody UserUpdateRequest request) {
+		userService.updateUserName(request);
+	}
 
-  @DeleteMapping("/user")
-  public void deleteUser(@RequestParam String name) {
-    userService.deleteUser(name);
-  }
+	@DeleteMapping("/user")
+	public void deleteUser(@RequestParam String name) {
+		userService.deleteUser(name);
+	}
 
 }
