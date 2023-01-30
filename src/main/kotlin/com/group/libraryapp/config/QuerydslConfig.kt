62b06a4b823bbf61d3c300d854fe4cb1bbad8dad
yzzzzun <yzzzzun@gmail.com>
2023-01-30 21:06:30 +0900

    feat: user loan history 서비스 querydsl 적용

---
+++ src/main/kotlin/com/group/libraryapp/config/QuerydslConfig.kt
@@ -0,0 +1,14 @@
+package com.group.libraryapp.config
+
+import com.querydsl.jpa.impl.JPAQueryFactory
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import javax.persistence.EntityManager
+
+@Configuration
+class QuerydslConfig(
+    private val em: EntityManager,
+) {
+    @Bean
+    fun querydsl(): JPAQueryFactory = JPAQueryFactory(em)
+}
